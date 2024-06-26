fc3a59f79aefb425486db3bbc62c1cc08057e5e3
Jono <127705274+KangJ0n0@users.noreply.github.com>
2024-06-26 20:36:40 +0700

    test deploy

--- app.js
+++ app.js
@@ -14,10 +14,21 @@ app.set("view engine", "ejs");
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
-});
